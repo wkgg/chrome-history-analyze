bfb7a087ae8b38b3e0dc63bcf70d01f4eaed1788
Amos <wky1991@gmail.com>
2015-11-04 22:57:57 +0800

    add last visited time

--- popup.js
+++ popup.js
@@ -17,13 +17,19 @@ function getCurrentTabUrl(callback) {
     callback(url);
   });
 }
+
 function getVisitedTimes(url, callback) {
   var domain = url.split('/')[2].split(':')[0].replace(/(www\.)|(mail\.)|(map\.)|(wiki\.)/, '')
-  chrome.history.search({text:domain, startTime:1000000 ,maxResults:1000000}, function(results){
+  chrome.history.search({text:domain, startTime:1 ,maxResults:1000000}, function(results){
+    renderLastVisited(results[1].lastVisitTime);
     callback(results.length);
   });
 }
 
+function renderLastVisited(epochTime) {
+  var timeAgo = moment(epochTime).fromNow();  
+  document.getElementById('lastVisited').textContent = "lastVisited: " + timeAgo;
+}
 
 function renderUrl(statusText) {
   document.getElementById('url').textContent = statusText;
